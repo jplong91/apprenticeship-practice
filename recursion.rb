343545b32b0a5dee8615830ea229c99c04cd2098
John Long <jplong91@gmail.com>
2018-03-16 00:10:28 -0500

    recursion working with puts statements but not a single total for final 3 problems

--- recursion.rb
+++ recursion.rb
@@ -13,10 +13,28 @@ def even_numbers(index)
 end
 
 def sum_numbers(index, sum)
-  puts sum += index
+  sum += index
   if index < 1000
     sum_numbers(index + 1, sum)
   end
 end
 
-sum_numbers(1, 0)
+# sum_numbers(1, 0)
+
+def array_sum(array, index, sum)
+  sum += array[index]
+  if index < array.length - 1
+    array_sum(array, index + 1, sum)
+  end
+end
+
+# array_sum([1, 2, 3, 5, 8, 31], 0, 0)
+
+def factorial(number, product)
+  product *= number
+  if number > 1
+    factorial(number - 1, product)
+  end
+end
+
+# p factorial(5, 1)
