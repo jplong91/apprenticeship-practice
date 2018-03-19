6571c6d837412f8da2eb3418973b10e7b5b7ac7a
John Long <jplong91@gmail.com>
2018-03-19 17:42:28 -0500

    recursion complete with proper print commands

--- recursion.rb
+++ recursion.rb
@@ -14,24 +14,27 @@ end
 
 def sum_numbers(index, sum)
   sum += index
+  index == 1000 ? (return sum) : "continue recurring"
   if index < 1000
     sum_numbers(index + 1, sum)
   end
 end
 
-# sum_numbers(1, 0)
+# p sum_numbers(1, 0)
 
 def array_sum(array, index, sum)
   sum += array[index]
+  index == array.length - 1 ? (return sum) : "continue recurring"
   if index < array.length - 1
     array_sum(array, index + 1, sum)
   end
 end
 
-# array_sum([1, 2, 3, 5, 8, 31], 0, 0)
+# p array_sum([1, 2, 3, 5, 8, 31], 0, 0)
 
 def factorial(number, product)
   product *= number
+  number == 1 ? (return product) : "continue recurring"
   if number > 1
     factorial(number - 1, product)
   end
