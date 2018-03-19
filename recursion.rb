57afddc920f6fae820e269918b53bf9a9257dd5a
John Long <jplong91@gmail.com>
2018-03-19 18:02:46 -0500

    refactored problems 3-5

--- recursion.rb
+++ recursion.rb
@@ -13,31 +13,19 @@ def even_numbers(index)
 end
 
 def sum_numbers(index, sum)
-  sum += index
-  index == 1000 ? (return sum) : "continue recurring"
-  if index < 1000
-    sum_numbers(index + 1, sum)
-  end
+  index <= 1000 ? sum_numbers(index + 1, sum + index) : (return sum)
 end
 
 # p sum_numbers(1, 0)
 
 def array_sum(array, index, sum)
-  sum += array[index]
-  index == array.length - 1 ? (return sum) : "continue recurring"
-  if index < array.length - 1
-    array_sum(array, index + 1, sum)
-  end
+  index < array.length ? array_sum(array, index + 1, sum + array[index]) : (return sum)
 end
 
 # p array_sum([1, 2, 3, 5, 8, 31], 0, 0)
 
 def factorial(number, product)
-  product *= number
-  number == 1 ? (return product) : "continue recurring"
-  if number > 1
-    factorial(number - 1, product)
-  end
+  number > 1 ? factorial(number - 1, product * number) : (return product)
 end
 
 # p factorial(5, 1)
